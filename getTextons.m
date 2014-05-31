8fd17cf756ed1f69f5ff6aa952b3197d04d96e83
Sangdon Park <sangdonp@v05-64.seas.upenn.edu>
2014-05-31 15:33:33 -0400

    obtain textons in scale space

--- getTextons.m
+++ getTextons.m
@@ -1,16 +1,53 @@
-function [ o_textons ] = getTextons( i_trainImgs, i_sampleStep, i_K, i_fb )
+% function [ o_textons ] = getTextons( i_trainImgs, i_sampleStep, i_K, i_fb )
+% %GETTEXTON Summary of this function goes here
+% %   Detailed explanation goes here
+% 
+% %% get filter responses
+% disp('* getting filter responses...');
+% data_cell = cell(size(i_trainImgs, 1), size(i_trainImgs, 2));
+% for imgInd=1:size(i_trainImgs, 1)
+%     fprintf('- %d/%d\n', imgInd, size(i_trainImgs, 1));
+%     smapleStep = i_sampleStep;
+%     data_cell_s = cell(1, size(i_trainImgs, 2));
+%     for sInd=1:size(i_trainImgs, 2)
+%         curImg = i_trainImgs{imgInd, sInd};
+%         curResp = zeros(size(curImg, 1), size(curImg, 2), size(i_fb, 3));
+%         for fInd=1:size(i_fb, 3)
+%             curResp(:, :, fInd) = conv2(curImg, i_fb(:, :, fInd), 'same');
+%         end
+%         data_cell_s{1, sInd} = reshape(curResp, [size(curResp, 1)*size(curResp, 2) size(curResp, 3)]);
+%         data_cell_s{1, sInd} = data_cell_s{1, sInd}(1:round(smapleStep(sInd)):end, :);
+%     end
+%     data_cell(imgInd, :) = data_cell_s;
+% end
+
+function [ o_textons ] = getTextons( i_trainImgs, i_sampleStep, i_scales, i_K, i_fb )
 %GETTEXTON Summary of this function goes here
 %   Detailed explanation goes here
 
+nTrImg = size(i_trainImgs, 4);
+nScale = numel(i_scales);
+%%
+trainImgs_cell = cell(nTrImg, nScale);
+parfor i=1:nTrImg
+    curImgs = cell(1, nScale);
+    curImg = im2double(rgb2gray(i_trainImgs(:, :, :, i)));
+    tmppy = buildImgPyramid(i_scales, curImg, []);
+    for s=1:nScale
+        curImgs{s} = tmppy(s).img;
+    end
+    trainImgs_cell(i, :) = curImgs;
+end
+
 %% get filter responses
 disp('* getting filter responses...');
-data_cell = cell(size(i_trainImgs, 1), size(i_trainImgs, 2));
-for imgInd=1:size(i_trainImgs, 1)
-    fprintf('- %d/%d\n', imgInd, size(i_trainImgs, 1));
+data_cell = cell(size(trainImgs_cell, 1), size(trainImgs_cell, 2));
+for imgInd=1:size(trainImgs_cell, 1)
+    fprintf('- %d/%d\n', imgInd, size(trainImgs_cell, 1));
     smapleStep = i_sampleStep;
-    data_cell_s = cell(1, size(i_trainImgs, 2));
-    for sInd=1:size(i_trainImgs, 2)
-        curImg = i_trainImgs{imgInd, sInd};
+    data_cell_s = cell(1, size(trainImgs_cell, 2));
+    for sInd=1:size(trainImgs_cell, 2)
+        curImg = trainImgs_cell{imgInd, sInd};
         curResp = zeros(size(curImg, 1), size(curImg, 2), size(i_fb, 3));
         for fInd=1:size(i_fb, 3)
             curResp(:, :, fInd) = conv2(curImg, i_fb(:, :, fInd), 'same');
